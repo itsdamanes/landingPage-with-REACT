e0c44139f9652c1f8cc7ebee0131872647c07da6
Ignacio <ignacio.damanes@gmail.com>
2023-08-04 13:09:25 -0400

    avance commit 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import Home from './pages/home'
 import './styles/index.css'
-
+import 'bootstrap/'
 const root = createRoot(document.getElementById('root'));
 root.render(<Home />)
