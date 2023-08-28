4f2db1c522469a7a438089a3a1ea1eb83e258db4
Ignacio <ignacio.damanes@gmail.com>
2023-08-28 19:56:42 -0400

    errores finales

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 export const Header = () => {
     return (
         <>
-            <div>
-                <h1>A war welcome</h1>
-                <h4>Lorem Ipsumn tas ir teksta salikums, kuru izmanto poligrāfijā un maketēšanas darbos. Lorem Ipsum ir kļuvis par vispārpieņemtu teksta aizvietotāju kopš 16. gadsimta sākuma. Tajā laikā kāds nezināms iespiedējs izveidoja teksta fragmentu, lai nodrukātu grāmatu ar burtu paraugiem.</h4>
+        <div className='btn btn-light'>
+
+        
+            <div className="btn btn-secondary">
+                <h1 className='text-start'>A war welcome</h1>
+                <h4 className='text-sm-start"'>Lorem Ipsumn tas ir teksta salikums, kuru izmanto poligrāfijā un maketēšanas darbos. Lorem Ipsum ir kļuvis par vispārpieņemtu teksta aizvietotāju kopš 16. gadsimta sākuma. Tajā laikā kāds nezināms iespiedējs izveidoja teksta fragmentu, lai nodrukātu grāmatu ar burtu paraugiem.</h4>
                 <button type="button" className="btn btn-primary">Call to action!</button>
             </div>
+
+        </div>
         </>
     )
 }
